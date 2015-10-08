c2b490a427eee2e79e91a6db9507b9fcef39b37b
Der Gliech <gliech@users.noreply.github.com>
2015-10-08 13:10:37 +0000

    added metadata
    Added Debian init info and a script header. Also the script explicitly
    uses bash now.

--- craftbukkit_screen.sh
+++ craftbukkit_screen.sh
@@ -1,9 +1,28 @@
-#!/bin/sh
-# Controls the minecraft server
+#!/bin/bash -
 
-#####################
-#START CONFIGURATION#
-#####################
+### BEGIN INIT INFO
+# Provides:          craftbukkit
+# Required-Start:    $remote_fs $syslog $local_fs $network $time
+# Required-Stop:     $remote_fs $syslog $local_fs $network $time
+# Should-Start:      $named
+# Should-Stop:       $named
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Manages Craftbukkit
+# Description:       Launches and Controls Craftbukkit in the background with the help of GNU screen.
+### END INIT INFO
+
+### BEGIN SCRIPT HEADER
+# title:             craftbukkit_screen.sh
+# description:       Launches and Controls Craftbukkit in the background with the help of GNU screen.
+# author:            gliech
+# date:              20151008
+# notes:             This script has been released to the public domain under http://pastebin.com/tQBm2xWv I only take credit for the changes I made myself.
+# bash_version:      4.2.25(1)-release
+### END SCRIPT HEADER
+
+
+### START CONFIGURATION
 
 # Replace the location path with the folder containing your CraftBukkit.jar or minecraft_server.jar file
 LOCATION="/home/username/minecraft"
@@ -17,9 +36,8 @@ JAVA="java"
 #Java Options - Replace with options that are sane and stable for your server
 JAVAOPTS="-server -jar"
 
-###################
-#END CONFIGURATION#
-###################
+### END CONFIGURATION
+
 
 #Determine whether or not Minecraft is already running
 RUNNING=`screen -ls | grep minecraft`
